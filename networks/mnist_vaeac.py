551a05a5ac008236b19cc0e6f7aa870aa9c3a8c9
rohitrango <rohit.rango@gmail.com>
2019-03-17 19:36:45 +0530

    Wrote network for mnist -> working

---
+++ networks/mnist_vaeac.py
@@ -0,0 +1,166 @@
+'''
+Network for MNIST
+Author: Rohit Jena
+'''
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+import parts as parts
+
+class ProposalNetMini(nn.Module):
+    '''
+    This network is the prior net for MNIST
+    '''
+    def __init__(self,
+                 inp_channels=3,
+                 n_hidden=16,
+                 fc_hidden=50,
+                 fc_out=16,
+                 activation=F.relu,
+                ):
+        super(ProposalNetMini, self).__init__()
+        self.inp_channels = inp_channels
+        self.activation = activation
+        # Get modules
+        self.in_conv = parts.InConv(inp_channels, n_hidden)
+        self.resblock1 = parts.ResBlock(n_hidden, n_hidden) # 14 * 14
+        self.resblock2 = parts.ResBlock(n_hidden, n_hidden) # 7 * 7
+        self.maxpool1 = nn.MaxPool2d(2, stride=2)
+        self.maxpool2 = nn.MaxPool2d(2, stride=2)
+        # fc layers
+        self.fc1 = nn.Linear(n_hidden * 7 * 7, fc_hidden)
+        self.fc_mean = nn.Linear(fc_hidden, fc_out)
+        self.fc_sigma = nn.Linear(fc_hidden, fc_out)
+
+    def forward(self, image):
+        outx = torch.cat([image['image'], image['mask']], 1)
+        out = outx
+        out = self.in_conv(out)
+        out = self.resblock1(out)
+        out = self.maxpool1(out)
+        out = self.resblock2(out)
+        out = self.maxpool2(out)
+        out = out.view(out.shape[0], -1)
+        # fully connected parts
+        out = self.activation(self.fc1(out))
+        mean = self.fc_mean(out)
+        logs = self.fc_sigma(out)
+        return mean, logs
+
+
+class EncoderDecoderNetMini(nn.Module):
+    '''
+    Prior and generator network
+    '''
+    def __init__(self,
+                 cfg,
+                 activation=F.relu,
+                ):
+        super(EncoderDecoderNetMini, self).__init__()
+        model_cfg = cfg['model']
+        n_hidden = model_cfg['n_hidden']
+        fc_hidden = model_cfg['fc_hidden']
+        fc_out = model_cfg['fc_out']
+        inp_channels = model_cfg['inp_channels']
+        last_layer = model_cfg['last_layer']
+
+        # Store the last layer activation
+        if last_layer == 'tanh':
+            self.last_activation = F.tanh
+        elif last_layer == 'sigmoid':
+            self.last_activation = F.sigmoid
+        else:
+            self.last_activation = None
+
+        self.proposal_net = ProposalNetMini(
+            inp_channels=inp_channels,
+            n_hidden=n_hidden,
+            fc_hidden=fc_hidden,
+            fc_out=fc_out
+            )
+
+        self.n_hidden = n_hidden
+        # Prior and generator net
+        self.inp_channels = inp_channels
+        self.activation = activation
+        # Get modules
+        self.in_conv = parts.InConv(inp_channels, n_hidden)
+        self.resblock1 = parts.ResBlock(n_hidden, n_hidden) # 14 * 14
+        self.resblock2 = parts.ResBlock(n_hidden, n_hidden) # 7 * 7
+        self.maxpool1 = nn.MaxPool2d(2, stride=2)
+        self.maxpool2 = nn.MaxPool2d(2, stride=2)
+        # fc layers
+        self.fc1 = nn.Linear(n_hidden * 7 * 7, fc_hidden)
+        self.fc_mean = nn.Linear(fc_hidden, fc_out)
+        self.fc_sigma = nn.Linear(fc_hidden, fc_out)
+
+        # Decoder network
+        self.out_fc1 = nn.Linear(fc_out, fc_hidden)
+        self.out_fc2 = nn.Linear(fc_hidden, n_hidden * 7 * 7)
+        # resblocks
+        self.out_resblock1 = parts.ResBlock(2 * n_hidden, n_hidden)
+        self.out_resblock2 = parts.ResBlock(2 * n_hidden, n_hidden)
+        self.upsample = nn.Upsample(scale_factor=2)
+        # Final output
+        self.out_conv = nn.Conv2d(n_hidden, inp_channels, 3, padding=1)
+
+
+    def forward(self, image):
+        # Proposal net outputs
+        # proposal net will take care of what inputs to take
+        prop_mean, prop_logs = self.proposal_net(image)
+        # Prior net
+        # concatenate the observed image and mask together
+        outx = torch.cat([image['observed'], image['mask']], 1)
+        out = outx
+        out = self.in_conv(out)
+        out = self.resblock1(out)           # 28
+        out = self.maxpool1(out)            # 14
+        out1 = out
+        out = self.resblock2(out)           # 14
+        out = self.maxpool2(out)            # 7
+        out2 = out
+        out = out.view(out.shape[0], -1)
+        # fully connected parts
+        out = self.activation(self.fc1(out))
+        mean = self.fc_mean(out)
+        logs = self.fc_sigma(out)
+        sample = mean + torch.exp(logs)*torch.randn(mean.shape).to(mean.device)
+        # Decoder net
+        out = self.activation(self.out_fc1(sample))
+        out = self.activation(self.out_fc2(out))
+        out = out.view(out.shape[0], self.n_hidden, 7, 7)
+        # Conv
+        out = self.out_resblock1(out, out2)
+        out = self.upsample(out)
+        out = self.out_resblock2(out, out1)
+        out = self.upsample(out)
+        out = self.out_conv(out)
+        if self.last_activation is not None:
+            out = self.last_activation(out)
+
+        return {
+            'out'       : out,
+            'prop_mean' : prop_mean,
+            'prop_logs' : prop_logs,
+            'mean'      : mean,
+            'logs'      : logs,
+        }
+
+
+if __name__ == '__main__':
+    CFG = {
+        'model': {
+            'n_hidden': 16,
+            'fc_hidden': 50,
+            'fc_out': 16,
+            'inp_channels':1,
+        }
+    }
+    network = EncoderDecoderNetMini(CFG)
+    inputs = torch.rand(8, 1, 28, 28)
+    outputs = network(inputs)
+    print 'inputs', inputs.shape
+    for key, value in outputs.items():
+        print key, value.shape
---
+++ networks/parts.py
@@ -0,0 +1,74 @@
+'''
+Parts for networks
+Author: Rohit Jena
+'''
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+class ResBlock(nn.Module):
+    '''
+    Residual block with or without downsampling
+    '''
+    def __init__(self,
+                 in_ch,
+                 out_ch,
+                 hid_ch=None,
+                 kernel_size=3,
+                 padding=1,
+                 activation=F.relu
+                ):
+        super(ResBlock, self).__init__()
+        hid_ch = out_ch if hid_ch is None else hid_ch
+        self.in_ch = in_ch
+        self.out_ch = out_ch
+        self.hid_ch = hid_ch
+        self.activation = activation
+        # Define modules
+        self.conv1 = nn.Conv2d(in_ch, hid_ch, kernel_size=kernel_size, padding=padding)
+        self.conv2 = nn.Conv2d(hid_ch, out_ch, kernel_size=kernel_size, padding=padding)
+        self.residual = nn.Conv2d(in_ch, out_ch, kernel_size=kernel_size, padding=padding)
+
+        self.bn1 = nn.BatchNorm2d(hid_ch)
+        self.bn2 = nn.BatchNorm2d(out_ch)
+
+    def forward(self, image, concat=None):
+        # get output from image
+        if concat is None:
+            outx = image
+        else:
+            outx = torch.cat([image, concat], 1)
+
+        out = self.bn1(self.conv1(outx))
+        out = self.activation(out)
+        out = self.bn2(self.conv2(out))
+        out = self.activation(out)
+        out = out + self.residual(outx)
+        return out
+
+
+class InConv(nn.Module):
+    '''
+    Just the initial layers
+    '''
+    def __init__(self,
+                 in_ch,
+                 out_ch,
+                 kernel_size=3,
+                 padding=1,
+                 activation=F.relu
+                ):
+        super(InConv, self).__init__()
+        self.in_ch = in_ch
+        self.out_ch = out_ch
+        # Modules
+        self.bn0 = nn.BatchNorm2d(in_ch)
+        self.conv1 = nn.Conv2d(in_ch, out_ch, kernel_size, padding=padding)
+        self.bn1 = nn.BatchNorm2d(out_ch)
+        self.activation = activation
+
+    def forward(self, image):
+        out = self.bn0(image)
+        out = self.bn1(self.conv1(out))
+        out = self.activation(out)
+        return out
